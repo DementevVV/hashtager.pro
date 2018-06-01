a38ab4c645c6c62691336b7a64dc924a4990f7ee
DementevVV <dem_dvv@mail.ru>
2018-06-01 16:07:09 +0300

    basic functional

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('StarterApp', ['ngclipboard', 'ngMaterial', 'ui.router', 'ngAnimate']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider.state('index', {
+      url: "/",
+      templateUrl: 'flow.html'
+  });
+});
+
+app.controller('MenuCtrl', function ($scope, $mdSidenav, $log) {
+
+  $scope.toggleLeft = buildToggler('left');
+  $scope.isOpenLeft = function(){
+    return $mdSidenav('left').isOpen();
+  };
+  function buildToggler(navID) {
+    return function() {
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+        });
+    };
+  }
+});
+app.controller('LeftCtrl', function ($scope, $mdSidenav, $log) {
+  $scope.close = function () {
+    $mdSidenav('left').close()
+      .then(function () {
+      });
+  };
+});
+
+app.srefAndCloseSideNav = function(stateName,$state,$mdSidenav){
+    $state.go(stateName);
+    $mdSidenav('left').close();
+}
